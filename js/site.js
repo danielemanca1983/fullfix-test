8a8b7c2f21dea33cd7b7381b34965e7bb5f52f67
Daniele Manca <danmanc83@yahoo.co.uk>
2016-02-07 18:08:37 +0000

    Added js code to inject json data into the markup for the slides, slightly edited home.json object titles

--- js/site.js
+++ js/site.js
@@ -1,4 +1,19 @@
-$('.home-slider').flickity({
-  prevNextButtons: false,
-  contain: true
+$.getJSON("home.json", function (slideData) {
+    var slides = slideData.slides;
+    var $slideTemplate = $(".templates .slide");
+
+    var $slideItems = slides.map(function (current) {
+        var $newSlide = $slideTemplate.clone();
+        $(".slide-title", $newSlide).text(current.title);
+        $(".thumb", $newSlide).css("background-image", "url(" + current.image + ")");
+        $(".slide-url", $newSlide).attr("href", current.url);
+        return $newSlide;
+    });
+
+    $(".home-slider").html($slideItems);
+
+    $('.home-slider').flickity({
+      prevNextButtons: false,
+      contain: true
+    });
 });
